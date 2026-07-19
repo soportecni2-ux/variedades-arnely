4a51fee935a072c46f99c4aaae400ff99a4723ad
soportecni2-ux <soportecni2@gmail.com>
2026-07-19 11:56:15 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,74 @@
-// Se completará en el siguiente paquete
+// ======================================
+// VARIEDADES ARNELY PRO 2.0
+// Service Worker
+// ======================================
+
+const CACHE_NAME = "arnely-pro-v1";
+
+const archivos = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json"
+];
+
+// Instalar
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+        .then(cache => {
+
+            return cache.addAll(archivos);
+
+        })
+
+    );
+
+});
+
+// Activar
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys => {
+
+            return Promise.all(
+
+                keys.map(key => {
+
+                    if(key !== CACHE_NAME){
+
+                        return caches.delete(key);
+
+                    }
+
+                })
+
+            );
+
+        })
+
+    );
+
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+        .then(response => {
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
+});
